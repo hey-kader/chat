494e5519f0100b6c06ac40dbd352a3fb410ace34
= <=>
2022-03-14 03:12:35 -0400

    server resources color and font

---
+++ server/font.lua
@@ -0,0 +1,6 @@
+font = love.graphics.newImageFont("imagefont.png", 
+	" abcdefghijklmnopqrstuvwxyz" ..
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
+	"123456789.,!?-+/():;%&`'*#=[]\"")
+
+love.graphics.setFont(font, 10)
